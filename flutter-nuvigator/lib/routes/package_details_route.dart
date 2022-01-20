2cc4259963d365d77fedebba5278c2d4a20c7996
Yuka Solano <yukakyushima@gmail.com>
2022-01-19 22:45:49 -0300

    Curso 9 - Aula 3 - Params Parser

--- flutter-nuvigator/lib/routes/package_details_route.dart
+++ flutter-nuvigator/lib/routes/package_details_route.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:nuvigator/next.dart';
+import 'package:proj/models/package_model.dart';
+import 'package:proj/models/producer_model.dart';
 import 'package:proj/screens/package_details_screen.dart';
 
-class PackageDetailsRoute extends NuRoute {
+class PackageDetailsArgs {
+  final Producer producer;
+  final Package package;
+
+  PackageDetailsArgs({this.producer, this.package});
+
+  static PackageDetailsArgs fromJson(Map<String, dynamic> json) {
+    return PackageDetailsArgs(
+        producer: json['producer'], package: json['package']);
+  }
+}
+
+class PackageDetailsRoute extends NuRoute<NuRouter, PackageDetailsArgs, String> {
   @override
   String get path => 'package-details';
 
@@ -10,9 +24,12 @@ class PackageDetailsRoute extends NuRoute {
   ScreenType get screenType => materialScreenType;
 
   @override
-  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
+  ParamsParser<PackageDetailsArgs> get paramsParser => PackageDetailsArgs.fromJson;
+  
+  @override
+  Widget build(BuildContext context, NuRouteSettings<PackageDetailsArgs> settings) {
     return PackageDetailsScreen(
-        package: settings.rawParameters['package'],
-        producer: settings.rawParameters['producer']);
+        package: settings.args.package,
+        producer: settings.args.producer);
   }
 }
