47c038ac4d559325b581dc507c0084dead528648
Yuka Solano <yukakyushima@gmail.com>
2022-02-18 13:30:18 -0300

    Curso 10 - Aula 2

--- flutter-notifications-start/lib/main.dart
+++ flutter-notifications-start/lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:meetups/http/web.dart';
+import 'package:meetups/models/device.dart';
 import 'package:meetups/screens/events_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  String? token = await messaging.getToken();
+  print('TOKEN $token');
+  setPushToken(token);
+  runApp(App());
+}
+
+void setPushToken(String? token) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? prefsToken = prefs.getString('pushToken');
+  bool? prefSent = prefs.getBool('tokenSent');
+  print('Token: $prefsToken - Sent? $prefSent');
+
+  if (prefsToken != token || (prefsToken == token && prefSent == false)) {
+    print('Enviando o token para o servidor');
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String? brand;
+    String? model;
+
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      print('Rodando no ${androidInfo.model}');
+      model = androidInfo.model;
+      brand = androidInfo.brand;
+    } else {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      print('Rodando no ${iosInfo.utsname.machine}');
+      model = iosInfo.utsname.machine;
+      brand = 'Apple';
+    }
+    Device device = Device(brand: brand, model: model, token: token);
+    sendDevice(device);
+  }
+}
 
 class App extends StatelessWidget {
   @override
